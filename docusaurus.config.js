87e46f57adc1001d4c8859be851eeb005408fe33
João Pedro Lucatto <joaopedro.ayruth.lucatto@gmail.com>
2026-04-21 10:53:24 -0300

    refactor: config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,41 +1,19 @@
-// @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
 import { themes as prismThemes } from 'prism-react-renderer';
-
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
     title: 'Blog',
     tagline: 'Tecnologia, dicas e pensamentos aleatórios de quem vive no LOGGER_LEVEL=\'debug\'.',
-    favicon: 'img/favicon.ico',
-
-    // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+    favicon: 'https://github.com/joaopedrolucatto.png',
     future: {
         v4: true, // Improve compatibility with the upcoming Docusaurus v4
     },
-
-    // Set the production url of your site here
     url: 'https://blog.joaopedrolucatto.com.br',
-    // Set the /<baseUrl>/ pathname under which your site is served
-    // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/blog/',
-    deploymentBranch: 'main',
-
-    // GitHub pages deployment config.
-    // If you aren't using GitHub pages, you don't need these.
+    baseUrl: '/',
     organizationName: 'JoaoPedroLucatto', // Usually your GitHub org/user name.
     projectName: 'blog', // Usually your repo name.
 
     onBrokenLinks: 'throw',
-
-    // Even if you don't use internationalization, you can use this field to set
-    // useful metadata like html lang. For example, if your site is Chinese, you
-    // may want to replace "en" with "zh-Hans".
     i18n: {
         defaultLocale: 'en',
         locales: ['en'],
@@ -48,8 +26,6 @@ const config = {
             ({
                 docs: {
                     sidebarPath: './sidebars.js',
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
                     editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
                 },
                 blog: {
